746bc39af1271aaf72aae840b5feaaf691e34f2b
Evgeniy <ev.shipitsyn@yandex.ru>
2024-10-10 11:42:58 +0300

    Initial Commit

---
+++ GuessNumber/GuessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberApp.swift
+//  GuessNumber
+//
+//  Created by Евгений on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
